1b2a856ba0e6fd1513ee8d1c0dfd36515f6d5975
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-10 09:33:33 -0500

    implement functions

--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Some String" // Fully Long Form
-	card := "Some String"
-	card = "New String"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "New String"
 }
