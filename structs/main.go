1e2defa3706220122b610d4991e7fbc38b1eb554
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-15 12:06:46 -0500

    update struct values

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-
+	var john person
+	john.firstName = "John"
+	john.lastName = "Vann"
+	fmt.Println(john)
+	fmt.Printf("%+v\n", john)
 }
