d6e63e7f47e6e6cbf49ee38fdf05c09b32e347ad
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-15 12:41:34 -0500

    embed structs

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var john person
-	john.firstName = "John"
-	john.lastName = "Vann"
-	fmt.Println(john)
+	john := person{
+		firstName: "John",
+		lastName:  "Vann",
+		contact: contactInfo{
+			email:   "john@gmail.com",
+			zipCode: 95000,
+		},
+	}
+
 	fmt.Printf("%+v\n", john)
 }
