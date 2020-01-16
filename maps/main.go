5a05423e2b041c0a6c5bb2f8ff7b3b518bb7e0cf
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-16 09:29:20 -0500

    add another way to declare a map and manipulate it

--- maps/main.go
+++ maps/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
+	// var colors map[string]string
+
+	/* colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#50C878",
-	}
+	} */
+
+	colors := make(map[int]string)
+	colors[10] = "#ff00ff"
+	colors[20] = "#ffffff"
+
+	delete(colors, 10)
 	fmt.Println(colors)
 }
