59a70ea678baa3595a2c892d37812563b9343a30
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-31 13:13:43 -0500

    move sleep out of main

--- channels/statusChecker/main.go
+++ channels/statusChecker/main.go
@@ -22,12 +22,12 @@ func main() {
 	}
 
 	for l := range c {
-		time.Sleep(5 * time.Second)
 		go checkLink(l, c)
 	}
 }
 
 func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Second)
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
